7089903ad385faec7de69bd70915245d33cc973d
deividdmc <88162752+deividdmc@users.noreply.github.com>
2023-05-24 14:14:41 -0300

    Divisiveis

--- atividadeLuidyMoura.js
+++ atividadeLuidyMoura.js
@@ -5,8 +5,8 @@ arrayOut = ['']
 montaArray =(valor, arrayOut) =>{
     for(let i= 1; i <= valor; i++){
         aux = ""
-        if(i%5 == 0) aux = "Luidy"
-        if(i%9 == 0) aux = aux+"Moura"
+        if(i%5 == 0) aux = "divisivel por 5"
+        if(i%9 == 0) aux = aux+"divisivel por 9"
         if(!aux == ""){
             arrayOut.push(aux)
         } else arrayOut.push(i)
